ef6786f0be4d7f55f7cafd4d05bd96a43325ae2b
Fábio Braz <fabiobrazferreira@gmail.com>
2023-04-18 17:48:47 -0300

    update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@popperjs/core';
 import App from './App';
 
 ReactDOM.render(
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+    <h1>Olá Mundo</h1>
+    </>
+  );
+};
+
+export default Home;
